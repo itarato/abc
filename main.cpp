d29e922a952b134500a9e883bd0ef6bac78b4754
Peter Arato <it.arato@gmail.com>
2021-04-04 15:36:04 -0400

    Sound stoppable.

--- main.cpp
+++ main.cpp
@@ -43,28 +43,31 @@ class SoundEffect {
   SDL_AudioSpec wav_spec;
   Uint32 wav_length;
   Uint8 *wav_buffer;
-  SDL_AudioDeviceID deviceId;
+  SDL_AudioDeviceID device_id;
 
  public:
   SoundEffect(const char *effect_file_name) {
     if (!SDL_LoadWAV(effect_file_name, &wav_spec, &wav_buffer, &wav_length))
       PANIC("Cannot load wav");
 
-    deviceId = SDL_OpenAudioDevice(NULL, 0, &wav_spec, NULL, 0);
-    if (!deviceId) PANIC("Cannot open audio device");
+    device_id = SDL_OpenAudioDevice(NULL, 0, &wav_spec, NULL, 0);
+    if (!device_id) PANIC("Cannot open audio device");
   }
 
   ~SoundEffect() {
-    SDL_CloseAudioDevice(deviceId);
+    SDL_CloseAudioDevice(device_id);
     SDL_FreeWAV(wav_buffer);
   }
 
   void play() {
-    if (SDL_QueueAudio(deviceId, wav_buffer, wav_length) == -1)
+    stop();
+    if (SDL_QueueAudio(device_id, wav_buffer, wav_length) == -1)
       PANIC("Cannot queue audio");
 
-    SDL_PauseAudioDevice(deviceId, 0);
+    SDL_PauseAudioDevice(device_id, 0);
   }
+
+  void stop() { SDL_ClearQueuedAudio(device_id); }
 };
 
 class Image {
